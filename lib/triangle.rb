f16c6cf9a8bca01b6f3985c1f81b38494a120dd1
Leslie G <lesliegonzalez@me.com>
2020-09-01 23:41:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(kind)
-    kind.each {|key, value| self.send(("#{key}="), value)} 
+    @kind = kind
   end
   
   
